195f22896968e48e82c4b5996bb43c060012fc9f
James Sinclair <sinclair.j@gmail.com>
2024-12-19 19:21:28 +1100

    add averaging

--- src/main.cpp
+++ src/main.cpp
@@ -1,17 +1,13 @@
 /*
- * Test all gamepad buttons, axes and dpad
- */
-/*
- * Based off Teaching Tech's arduino sketch for spacemouse and the BLE Gamepad
- * example from the ESP32 BLE Arduino library
+ * Based off Teaching Tech's arduino sketch for spacemouse 
  */
 
 #include <Arduino.h>
 #include <BleCompositeHID.h>
 #include <GamepadDevice.h>
-// #include <BleGamepad.h>
 
 // TODO: probably revert back to BleGamepad.h
+// #include <BleGamepad.h>
 
 // Debugging
 // 0: Debugging off. Set to this once everything is working.
@@ -35,13 +31,6 @@ bool invRX = true;   // Rotate around X axis (tilt front/back)
 bool invRY = false;  // Rotate around Y axis (tilt left/right)
 bool invRZ = true;   // Rotate around Z axis (twist left/right)
 
-// const int potPin = 0;                // Potentiometer is connected to GPIO 0
-// // old : 34 (Analog ADC1_CH6) const int numberOfPotSamples = 5;     // Number
-// of pot samples to take (to smooth the values) const int delayBetweenSamples =
-// 4;    // Delay in milliseconds between pot samples const int
-// delayBetweenHIDReports = 5; // Additional delay in milliseconds between HID
-// reports
-
 // Default Assembly when looking from above:
 //    C           Y+
 //    |           .
@@ -62,19 +51,6 @@ int MULTIPLEXER_SIG = A0;
 // number of potentiometer axes that are read
 int NUM_AXES = 8;
 
-// Wiring
-// int PINLIST[] = {
-//     // The positions of the reads
-//     A1,  // X-axis A
-//     A0,  // Y-axis A
-//     A3,  // X-axis B
-//     A2,  // Y-axis B
-//          //   A7, // X-axis C
-//          //   A6, // Y-axis C
-//          //   A9, // X-axis D
-//          //   A8  // Y-axis D
-// };
-
 // Deadzone to filter out unintended movements. Increase if the mouse has small
 // movements when it should be idle or the mouse is too senstive to subtle
 // movements.
@@ -89,21 +65,38 @@ int RX_SENSITIVITY = 1;
 int RY_SENSITIVITY = RX_SENSITIVITY;
 int RZ_SENSITIVITY = 1;
 
+int NUM_SAMPLES = 3;  // Number of pot samples to take (to smooth the values)
+// int SAMPLE_DELAY = 2;  // Delay in milliseconds between pot samples
+int MAIN_LOOP_DELAY = 50;  // Delay in milliseconds between HID reports
+
 // Centerpoint variable to be populated during setup routine - stores idle
 // position of each axis.
 int centerPoints[8];
 
+
 // Function to read and store analogue voltages for each joystick axis.
-// TODO: averaging
+// Takes average of NUM_SAMPLES samples from each axis and stores in rawReads
 void readAllFromJoystick(int* rawReads) {
-  // multiplexer 
+  int potValues[NUM_AXES] = {0};  // Array to store pot readings multiplexer
+
+  // Populate readings
+  for (int j = 0; j < NUM_SAMPLES; j++) {
+    for (int i = 0; i < NUM_AXES; i++) {
+      // set channel
+      digitalWrite(MULTIPLEXER_CONTROL_A, bitRead(i, 0));
+      digitalWrite(MULTIPLEXER_CONTROL_B, bitRead(i, 1));
+      digitalWrite(MULTIPLEXER_CONTROL_C, bitRead(i, 2));
+      // take sample from the axis
+      potValues[i] += analogRead(MULTIPLEXER_SIG);
+    }
+
+  }
+
   for (int i = 0; i < NUM_AXES; i++) {
-    // set channel
-    digitalWrite(MULTIPLEXER_CONTROL_A, bitRead(i, 0));
-    digitalWrite(MULTIPLEXER_CONTROL_B, bitRead(i, 1));
-    digitalWrite(MULTIPLEXER_CONTROL_C, bitRead(i, 2));
-    // read value
-    rawReads[i] = analogRead(MULTIPLEXER_SIG);
+    // compute average and store in array
+    rawReads[i] = potValues[i] / NUM_SAMPLES;
+    // reset potValues
+    potValues[i] = 0;
   }
 }
 
@@ -114,19 +107,7 @@ BleCompositeHID compositeHID;
 GamepadDevice* gamepad;
 GamepadConfiguration bleGamepadConfig;
 
-// // Gamepad with Report ID 1 (Axes: X, Y, Z)
-// GamepadDevice *gamepadTranslation;
-// GamepadConfiguration gamepadTranslationConfig;
-//
-// // Gamepad with Report ID 2 (Axes: RX, RY, RZ)
-// GamepadDevice *gamepadRotation;
-// GamepadConfiguration gamepadRotationConfig;
-//
-// // Gamepad with Report ID 3 (Buttons: 24 buttons)
-// GamepadDevice *gamepadButtons;
-// GamepadConfiguration gamepadButtonsConfig;
-//
-// Axes are matched to multiplexer channel 
+// Axes are matched to multiplexer channel
 #define AX 1
 #define AY 0
 #define BX 3
@@ -137,48 +118,39 @@ GamepadConfiguration bleGamepadConfig;
 #define DY 6
 
 void setup() {
-  pinMode(MULTIPLEXER_CONTROL_A, OUTPUT); 
+  // Set up multiplexer pins
+  pinMode(MULTIPLEXER_CONTROL_A, OUTPUT);
   pinMode(MULTIPLEXER_CONTROL_B, OUTPUT);
   pinMode(MULTIPLEXER_CONTROL_C, OUTPUT);
   pinMode(MULTIPLEXER_SIG, INPUT);
 
+  // Set up serial
   Serial.begin(115200);
   Serial.println("Starting BLE work!");
 
+  // set up the HID report descriptor
   BLEHostConfiguration hostConfig;
   hostConfig.setVid(0x256f);
   hostConfig.setPid(0xc631);
 
   bleGamepadConfig.setAutoReport(false);
-  bleGamepadConfig.setControllerType(
-      CONTROLLER_TYPE_MULTI_AXIS);  // CONTROLLER_TYPE_JOYSTICK,
-                                    // CONTROLLER_TYPE_GAMEPAD (DEFAULT),
-                                    // CONTROLLER_TYPE_MULTI_AXIS
+  bleGamepadConfig.setControllerType( CONTROLLER_TYPE_MULTI_AXIS);  
   bleGamepadConfig.setButtonCount(numOfButtons);
   bleGamepadConfig.setHatSwitchCount(numOfHatSwitches);
-  // Some non-Windows operating systems and web based gamepad testers don't like
-  // min axis set below 0, so 0 is set by default
-  bleGamepadConfig.setAxesMin(
-      0x8001);  // -32767 --> int16_t - 16 bit signed integer - Can be in
-                // decimal or hexadecimal
-                // bleGamepadConfig.setAxesMin(0x0000); // 0 --> int16_t - 16
-                // bit signed integer - Can be in decimal or hexadecimal
-  bleGamepadConfig.setAxesMax(
-      0x7FFF);  // 32767 --> int16_t - 16 bit signed integer - Can be in decimal
-                // or hexadecimal
+  bleGamepadConfig.setAxesMin(0x8001);  // -32767 --> int16_t - 16 bit signed integer - Can be in  decimal or hexadecimal
+  bleGamepadConfig.setAxesMax(0x7FFF);  // 32767 --> int16_t - 16 bit signed integer - Can be in decimal or hexadecimal
 
-  gamepad = new GamepadDevice(
-      bleGamepadConfig);  // Simulation controls, special buttons and hats 2/3/4
-                          // are disabled by default
+  gamepad = new GamepadDevice(bleGamepadConfig);  
   compositeHID.addDevice(gamepad);
   compositeHID.begin(hostConfig);
-  // changing bleGamepadConfig after the begin function has no effect, unless
-  // you call the begin function again
+
+  // determine centre points
   readAllFromJoystick(centerPoints);
 }
 
 void loop() {
   int rawReads[NUM_AXES], centered[NUM_AXES];
+
   if (compositeHID.isConnected()) {
     // Serial.println("\nn--- Axes Decimal ---");
     // Serial.print("Axes Min: ");
@@ -190,8 +162,11 @@ void loop() {
     // Report back 0-1023 raw ADC 10-bit values if enabled
     if (debug == 1) {
       char buffer[200];
-      sprintf(buffer, "Raw - AX:%4d, AY:%4d, BX:%4d, BY:%4d, CX:%4d, CY:%4d, DX:%4d, DY:%4d",
-              rawReads[AX], rawReads[AY], rawReads[BX], rawReads[BY], rawReads[CX], rawReads[CY], rawReads[DX], rawReads[DY]);
+      sprintf(buffer,
+              "Raw - AX:%4d, AY:%4d, BX:%4d, BY:%4d, CX:%4d, CY:%4d, DX:%4d, "
+              "DY:%4d",
+              rawReads[AX], rawReads[AY], rawReads[BX], rawReads[BY],
+              rawReads[CX], rawReads[CY], rawReads[DX], rawReads[DY]);
       Serial.println(buffer);
     }
 
@@ -200,26 +175,36 @@ void loop() {
       centered[i] = rawReads[i] - centerPoints[i];
     }
 
-    // Report centered joystick values if enabled. Values should be approx -500 to +500, jitter around 0 at idle.
+    // Report centered joystick values if enabled. Values should be approx -500
+    // to +500, jitter around 0 at idle.
     if (debug == 2) {
       char buffer[200];
-      sprintf(buffer, "Centred - AX:%4d, AY:%4d, BX:%4d, BY:%4d, CX:%4d, CY:%4d, DX:%4d, DY:%4d",
-              centered[AX], centered[AY], centered[BX], centered[BY], centered[CX], centered[CY], centered[DX], centered[DY]);
+      sprintf(buffer,
+              "Centred - AX:%4d, AY:%4d, BX:%4d, BY:%4d, CX:%4d, CY:%4d, "
+              "DX:%4d, DY:%4d",
+              centered[AX], centered[AY], centered[BX], centered[BY],
+              centered[CX], centered[CY], centered[DX], centered[DY]);
       Serial.println(buffer);
     }
 
-    // Filter movement values. Set to zero if movement is below deadzone threshold.
+    // Filter movement values. Set to zero if movement is below deadzone
+    // threshold.
+    // TODO: offset values to start from deadzone?
     for (int i = 0; i < NUM_AXES; i++) {
       if (centered[i] < DEADZONE && centered[i] > -DEADZONE) {
         centered[i] = 0;
       }
     }
 
-    // Report centered joystick values. Filtered for deadzone. Approx -500 to +500, locked to zero at idle
+    // Report centered joystick values. Filtered for deadzone. Approx -500 to
+    // +500, locked to zero at idle
     if (debug == 3) {
       char buffer[200];
-      sprintf(buffer, "Centred/Filtered - AX:%4d, AY:%4d, BX:%4d, BY:%4d, CX:%4d, CY:%4d, DX:%4d, DY:%4d",
-              centered[AX], centered[AY], centered[BX], centered[BY], centered[CX], centered[CY], centered[DX], centered[DY]);
+      sprintf(buffer,
+              "Centred/Filtered - AX:%4d, AY:%4d, BX:%4d, BY:%4d, CX:%4d, "
+              "CY:%4d, DX:%4d, DY:%4d",
+              centered[AX], centered[AY], centered[BX], centered[BY],
+              centered[CX], centered[CY], centered[DX], centered[DY]);
       Serial.println(buffer);
     }
 
@@ -274,7 +259,8 @@ void loop() {
     // depending on the parameter.
     if (debug == 4) {
       char buffer[200];
-      sprintf(buffer, "Transformed - TX:%4d, TY:%4d, TZ:%4d, RX:%4d, RY:%4d, RZ:%4d",
+      sprintf(buffer,
+              "Transformed - TX:%4d, TY:%4d, TZ:%4d, RX:%4d, RY:%4d, RZ:%4d",
               transX, transY, transZ, rotX, rotY, rotZ);
       Serial.println(buffer);
     }
@@ -283,72 +269,21 @@ void loop() {
     // the arithmatic above.
     if (debug == 5) {
       char buffer[400];
-      sprintf(buffer, "AX:%4d, AY:%4d, BX:%4d, BY:%4d, CX:%4d, CY:%4d, DX:%4d, DY:%4d || Transformed - TX:%4d, TY:%4d, TZ:%4d, RX:%4d, RY:%4d, RZ:%4d",
-              centered[AX], centered[AY], centered[BX], centered[BY], centered[CX], centered[CY], centered[DX], centered[DY],
-              transX, transY, transZ, rotX, rotY, rotZ);
+      sprintf(buffer,
+              "AX:%4d, AY:%4d, BX:%4d, BY:%4d, CX:%4d, CY:%4d, DX:%4d, DY:%4d "
+              "|| Transformed - TX:%4d, TY:%4d, TZ:%4d, RX:%4d, RY:%4d, RZ:%4d",
+              centered[AX], centered[AY], centered[BX], centered[BY],
+              centered[CX], centered[CY], centered[DX], centered[DY], transX,
+              transY, transZ, rotX, rotY, rotZ);
       Serial.println(buffer);
     }
 
-    gamepad->setAxes(transX, transY, transZ, rotX, rotY,
-                     rotZ);  // Reset all axes to zero
+    gamepad->setAxes(transX, transY, transZ, rotX, rotY, rotZ);  
     gamepad->sendGamepadReport();
   } else {
-    Serial.println("BLE not connected");
-  }
-  delay(100);
-}
-//
-// void loop() {
-
-/*void loop()
-{
-    if (bleGamepad.isConnected())
-    {
-        int potValues[numberOfPotSamples]; // Array to store pot readings
-        int potValue = 0;                  // Variable to store calculated pot
-reading average
-
-        // Populate readings
-        for (int i = 0; i < numberOfPotSamples; i++)
-        {
-            potValues[i] = analogRead(potPin);
-            potValue += potValues[i];
-            delay(delayBetweenSamples);
-        }
-
-        // Calculate the average
-        potValue = potValue / numberOfPotSamples;
-
-        // Map analog reading from 0 ~ 4095 to 32737 ~ 0 for use as an axis
-reading int adjustedValue = map(potValue, 0, 4095, 32737, 0);
-
-        // Update X axis and auto-send report
-        bleGamepad.setX(adjustedValue);
-        delay(delayBetweenHIDReports);
-
-        // The code below (apart from the 2 closing braces) is for pot value
-degugging, and can be removed
-        // Print readings to serial port
-        Serial.print("Sent: ");
-        Serial.print(adjustedValue);
-        Serial.print("\tRaw Avg: ");
-        Serial.print(potValue);
-        Serial.print("\tRaw: {");
-
-        // Iterate through raw pot values, printing them to the serial port
-        for (int i = 0; i < numberOfPotSamples; i++)
-        {
-            Serial.print(potValues[i]);
-
-            // Format the values into a comma seperated list
-            if (i == numberOfPotSamples - 1)
-            {
-                Serial.println("}");
-            }
-            else
-            {
-                Serial.print(", ");
-            }
-        }
+    if (debug > 0) {
+      Serial.println("BLE not connected");
     }
-}*/
+  }
+  delay(MAIN_LOOP_DELAY);
+}
